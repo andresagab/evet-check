b0476d945f02ba501f9afb4e5656f04ac0a6a058
Andrés Angulo <andrescabj981@gmail.com>
2025-06-30 20:15:50 -0500

    Update developer credit in admin footer to reflect collaboration

--- resources/views/components/layouts/footers/admin-footer.blade.php
+++ resources/views/components/layouts/footers/admin-footer.blade.php
@@ -12,7 +12,7 @@
 >
     <blockquote class="block text-center text-sm break-words text-slate-400 w-full">
         <p>
-            Desarrollador por: <a href="https://www.linkedin.com/in/andresangulodev/" target="_blank" title="Click para ver el perfil del autor principal" class="text-blue-600 dark:text-blue-400 hover:underline transition ease-in-out duration-150">Andrés Angulo - Software Developer</a>
+            Desarrollador en colaboración con: <a href="https://www.linkedin.com/in/andresangulodev/" target="_blank" title="Click para ver el perfil del autor principal" class="text-blue-600 dark:text-blue-400 hover:underline transition ease-in-out duration-150">Andrés Angulo - Software Developer</a>
         </p>
         <a href="https://github.com/andresagab/evet-check?tab=MIT-1-ov-file#readme" target="_blank" title="Click para ver la licencia" class="text-center text-sm break-words text-slate-300 hover:underline">MIT License - {{ date('Y') }}</a>
     </blockquote>
